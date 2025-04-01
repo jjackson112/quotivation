3006658e5e99fc860390f40f06a52071281ba5bd
jjackson112 <jjackson11211@gmail.com>
2025-04-01 14:18:03 -0400

    Disappear messages

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,15 +1,25 @@
 import React from "react";
+import { useEffect } from "react";
 
 // destructure props from App.js
 // onClick handler to invoke removeMessage - nothing needed to pass to it
+// clear message after 1.5 seconds - useEffect, setTImeout, cleanup function
 const Message = ({messageText, removeMessage}) => {
+    useEffect (() => {
+        const timeout = window.setTimeout (() => {
+            removeMessage();
+        }, 1500)
+
+        return () => 
+            window.clearTimeout(timeout);
+        });
 
     return (
         <div className="message">
             <p>{messageText}</p>
             <span className="close-message" onClick={removeMessage}>X</span>
         </div>
-    )
-}
+    );
+};
 
 export default Message;
