76034ab1f62d3c82adebf6ddb224f4a2f7a448d2
jjackson112 <jjackson11211@gmail.com>
2025-04-01 12:23:49 -0400

    Message component

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Loader } from "react-feather";
 import Quotes from "./components/quotes/Quotes";
-import QuoteCard from "./components/quotes/QuoteCard";
 import FavoriteQuotes from "./components/quotes/FavoriteQuotes";
+import Message from "./components/Message";
 import "./App.css";
 
 function App() {
@@ -13,6 +13,7 @@ function App() {
   const [category, setCategory] = useState("All");
   const [favoriteQuotes, setFavoriteQuotes] = useState([]);
   const [messageText, setMessageText] = useState("");
+  const [showMessage, setShowMessage] = useState(false);
 
   const quotesUrl =
     "https://gist.githubusercontent.com/skillcrush-curriculum/6365d193df80174943f6664c7c6dbadf/raw/1f1e06df2f4fc3c2ef4c30a3a4010149f270c0e0/quotes.js";
@@ -22,9 +23,6 @@ function App() {
 
   // how to favorite quotes
   const maxFaves = 3;
-
-  // make info available to the user
-  const showMessage = false;
 
   const fetchQuotes = async () => {
     try {
---
+++ src/components/Message.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Message () => {
+    return (
+        <div className="message">
+            <p span="close-message">X</p>
+        </div>
+    )
+}
+
+export default Message;
