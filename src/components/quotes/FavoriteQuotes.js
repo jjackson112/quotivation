7ce79276f1e03970bcd5ae0ad5296bb904d06e11
jjackson112 <jjackson11211@gmail.com>
2025-05-08 22:16:56 -0400

    Improve the text below favorite quotes

--- src/components/quotes/FavoriteQuotes.js
+++ src/components/quotes/FavoriteQuotes.js
@@ -5,12 +5,22 @@ import FavoriteQuoteCard from "./FavoriteQuoteCard";
 // map over favorite quotes and output one favorite quote card for each favorite quote
 // key can be quote.id since it's unique, pass entire quote
 // add index to quote in map array and listPosition prop to FavoriteQuoteCard component
+//
 
 const FavoriteQuotes = ({ favoriteQuotes, maxFaves, removeFromFavorites }) => {
     return (
         <section className="favorite-quotes">   
              <div className="wrapper quotes">
                 <h3>Top 3 Favorite Quotes</h3>
+                <div className="favorite-quotes-description">
+                    {favoriteQuotes.length < maxFaves ? (
+                    <h3>You can add <strong>{maxFaves - favoriteQuotes.length}</strong> more quotes to your top favorites 
+                    {maxFaves - favoriteQuotes.length === 1 ? "" :"s"}by selecting from the options below.<br />
+                    Once you choose, they will appear here.</h3>
+                    ) : (
+                        <h3>You've reached the max of {maxFaves} favorite quotes. Remove one to add another.</h3>
+                    )}
+                </div>
                     {favoriteQuotes.length > 0 && (
                         <ul>
                             {favoriteQuotes.map((quote, index) => (
@@ -18,10 +28,6 @@ const FavoriteQuotes = ({ favoriteQuotes, maxFaves, removeFromFavorites }) => {
                             ))}
                         </ul>
                     )}
-                <div className="favorite-quotes-description">
-                    <p>You can add up to three favorites by selecting from the options below.<br />
-                    Once you choose, they will appear here.</p>
-                </div>
             </div>
         </section>
     );
